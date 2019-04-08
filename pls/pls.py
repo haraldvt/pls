95e4e56fbbb4e0900a9b876462e5d2f8c64f7b99
CongoBongo <htvl@solconmail.nl>
2019-04-08 20:37:48 +0000

    added css

--- pls/pls.py
+++ pls/pls.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import time
 
 app = Flask(__name__)
 
+
+@app.route('/')
+@app.route('/index')
+def index():
+  plscontext =  {
+    'date' :  time.strftime("%c"),
+    'user' : {'username': 'Harald'},
+    'version': '0.0.3'
+   }
+   
+  return render_template('index.html', plscontext = plscontext)
 
 @app.route('/pls')
 def pls():
